353ec49ea6701946697ffc68dec2ead583a338ac
Adrianfeloh1 <adrianfeloh1@gmail.com>
2023-08-15 20:24:40 -0500

    aplicando funcionalidad de la sidebar en mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
+
+import { useState } from "react";
 import { Sidebar } from "./components/shared/Sidebar";
 import { RiMenuFill, RiUser3Line, RiPieChartLine, RiAddLine } from "react-icons/ri";
 
 export const App = () => {
+
+  const [showMenu, setShowMenu] = useState(false)
+  const [showOrder, setShowOrder] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-      <Sidebar />
+      <Sidebar showMenu={showMenu} />
       {/* menu mobile */}
       <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-500
        py-4 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
-        <button>
+        <button className="p-2">
           <RiUser3Line />
         </button>
-        <button>
+        <button className="p-2">
           <RiAddLine />
         </button>
-        <button>
+        <button className="p-2">
           <RiPieChartLine />
         </button>
-        <button className="text-white">
+        <button onClick={toggleMenu} className="text-white bg-[#EC7C6A] p-2">
           <RiMenuFill />
         </button>
 
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -8,11 +8,13 @@ import {
   RiLogoutBoxRLine,
 } from "react-icons/ri";
 
-export const Sidebar = () => {
+export const Sidebar = ({showMenu}) => {
+
+
   return (
     <div
-      className="bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col
-     justify-between py-6 rounded-tr-xl rounded-br-xl z-50 -left-full"
+      className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col
+     justify-between py-6 rounded-tr-xl rounded-br-xl z-50 ${showMenu ? "left-0" : "-left-full"} `}
     >
       <div>
         <ul className=" pl-4">
