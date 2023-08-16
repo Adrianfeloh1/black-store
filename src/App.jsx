a6714cef9fb542f6d174ded4a20a9bae3a318e5a
Adrianfeloh1 <adrianfeloh1@gmail.com>
2023-08-15 19:59:05 -0500

    Barra inferior en mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export const App = () => {
     <div className="bg-[#262837] w-full min-h-screen">
       <Sidebar />
       {/* menu mobile */}
-      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400
+      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-500
        py-4 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
         <button>
           <RiUser3Line />
