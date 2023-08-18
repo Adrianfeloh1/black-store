ef470670f25e95a90d35cba016677bac30fcb92b
Adrianfeloh1 <adrianfeloh1@gmail.com>
2023-08-18 10:12:43 -0500

    Finalizando vistas tablet y desktop de las cards e iniciando el cart

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import {
   RiPieChartLine,
   RiAddLine,
   RiCloseLine,
-  RiSearchLine
+  RiSearchLine,
+  RiArrowDownSLine
 } from "react-icons/ri";
 
 export const App = () => {
@@ -39,12 +40,12 @@ export const App = () => {
         </button>
 
       </nav>
-      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8 ">
-        <div className=" lg:col-span-6">
+      <main className="lg:pl-32 grid grid-cols-1 lg:grid-cols-8 p-4 pb-32">
+        <div className=" lg:col-span-6 md:p-8">
           {/* Header */}
-          <header className="p-4">
+          <header className="">
             {/* Title and search */}
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 md:flex-row md:justify-between md:items-center">
               <div>
                 <h1 className="text-2xl text-gray-300">Jeager Resto</h1>
                 <p className="text-gray-500">20 de agosto 2023</p>
@@ -60,7 +61,8 @@ export const App = () => {
               </form>
             </div>
             {/* Menu */}
-            <nav className="text-gray-300 flex items-center justify-between border-b">
+            <nav className="text-gray-300 flex items-center justify-between border-b my-6
+            md:justify-start md:gap-8">
               <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#EC7C6A]
               before:left-0 before:rounded-full before:-bottom-[1px] text-[#EC7C6A]">Hot Dishes</a>
               <a href="#" className="py-2 pr-4">Cold Dishes</a>
@@ -68,8 +70,115 @@ export const App = () => {
               <a href="#" className="py-2 pr-4">Gill</a>
             </nav>
           </header>
+          {/* Title Content */}
+          <div className="flex items-center justify-between mb-16">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine /> Dine in
+            </button>
+          </div>
+          {/* Content */}
+          <div className="px-8 grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center
+            text-gray-300">
+              <img src="plato1.png" className="w-40 h-40 object-cover -mt-20 shadow-xl rounded-full"
+                alt="plato img" />
+
+              <h3 className="font-bold text-2xl">Speacy seasoned seafood noodles</h3>
+
+              <p className="">$2.29</p>
+              <p className="text-gray-500">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center
+            text-gray-300">
+              <img src="plato1.png" className="w-40 h-40 object-cover -mt-20 shadow-xl rounded-full"
+                alt="plato img" />
+
+              <h3 className="font-bold text-2xl">Speacy seasoned seafood noodles</h3>
+
+              <p className="">$2.29</p>
+              <p className="text-gray-500">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center
+            text-gray-300">
+              <img src="plato1.png" className="w-40 h-40 object-cover -mt-20 shadow-xl rounded-full"
+                alt="plato img" />
+
+              <h3 className="font-bold text-2xl">Speacy seasoned seafood noodles</h3>
+
+              <p className="">$2.29</p>
+              <p className="text-gray-500">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center
+            text-gray-300">
+              <img src="plato1.png" className="w-40 h-40 object-cover -mt-20 shadow-xl rounded-full"
+                alt="plato img" />
+
+              <h3 className="font-bold text-2xl">Speacy seasoned seafood noodles</h3>
+
+              <p className="">$2.29</p>
+              <p className="text-gray-500">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center
+            text-gray-300">
+              <img src="plato1.png" className="w-40 h-40 object-cover -mt-20 shadow-xl rounded-full"
+                alt="plato img" />
+
+              <h3 className="font-bold text-2xl">Speacy seasoned seafood noodles</h3>
+
+              <p className="">$2.29</p>
+              <p className="text-gray-500">20 Bowls available</p>
+            </div>
+            {/* Card */}
+            <div className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center
+            text-gray-300">
+              <img src="plato1.png" className="w-40 h-40 object-cover -mt-20 shadow-xl rounded-full"
+                alt="plato img" />
+
+              <h3 className="font-bold text-2xl">Speacy seasoned seafood noodles</h3>
+
+              <p className="">$2.29</p>
+              <p className="text-gray-500">20 Bowls available</p>
+            </div>
+
+          </div>
         </div>
-        <div className=" lg:col-span-2 fixed right-0 lg:static ">carrito</div>
+        {/* Shopping Cart */}
+        <div className="bg-[#1F1D2B] fixed right-0 top-0 w-full lg:static lg:col-span-2 h-full">
+          {/* Orders */}
+          <div className="relative pt-16 text-gray-300 p-8">
+            <RiCloseLine className="absolute left-4 top-4 p-3 box-content text-gray-300
+             bg-[#262837] rounded-full text-xl"/>
+            <h1 className="text-2xl my-4 mt-4">Orders #151416</h1>
+
+            {/* Pills */}
+            <div className="flex items-center gap-4 flex-wrap mb-8">
+              <button className="bg-[#EC7C6A] text-white py-2 px-4 rounded-xl">
+                Dine in
+              </button>
+              <button className=" text-[#EC7C6A] py-2 px-4 rounded-xl border border-gray-500">
+                To Go
+              </button>
+              <button className=" text-[#EC7C6A] py-2 px-4 rounded-xl border border-gray-500">
+                Delivery
+              </button>
+            </div>
+            {/* Cart */}
+            <div>
+              <div className="grid grid-cols-6">
+                <h5 className="col-span-4">Item</h5>
+                <h5>Qty</h5>
+                <h5>Price</h5>
+              </div>
+              {/* Product */}
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
