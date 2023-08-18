4a17747f0500f5b07e3fe1d9992fd4ce056bd369
Adrianfeloh1 <adrianfeloh1@gmail.com>
2023-08-17 19:41:17 -0500

    Dando estilos al navbar con before:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 
 import { useState } from "react";
 import { Sidebar } from "./components/shared/Sidebar";
-import { RiMenuFill, RiUser3Line, RiPieChartLine, RiAddLine, RiCloseLine } from "react-icons/ri";
+import {
+  RiMenuFill,
+  RiUser3Line,
+  RiPieChartLine,
+  RiAddLine,
+  RiCloseLine,
+  RiSearchLine
+} from "react-icons/ri";
 
 export const App = () => {
 
@@ -28,10 +35,42 @@ export const App = () => {
           <RiPieChartLine />
         </button>
         <button onClick={toggleMenu} className="text-white p-2">
-          { showMenu ? <RiCloseLine/> : <RiMenuFill />}
+          {showMenu ? <RiCloseLine /> : <RiMenuFill />}
         </button>
 
       </nav>
+      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8 ">
+        <div className=" lg:col-span-6">
+          {/* Header */}
+          <header className="p-4">
+            {/* Title and search */}
+            <div className="flex flex-col gap-4">
+              <div>
+                <h1 className="text-2xl text-gray-300">Jeager Resto</h1>
+                <p className="text-gray-500">20 de agosto 2023</p>
+              </div>
+              <form action="">
+                <div className="w-full relative">
+                  <RiSearchLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-300 text-2xl" />
+                  <input type="text" className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg
+                 text-gray-300 outline-none"
+                    placeholder="Search"
+                  />
+                </div>
+              </form>
+            </div>
+            {/* Menu */}
+            <nav className="text-gray-300 flex items-center justify-between border-b">
+              <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#EC7C6A]
+              before:left-0 before:rounded-full before:-bottom-[1px] text-[#EC7C6A]">Hot Dishes</a>
+              <a href="#" className="py-2 pr-4">Cold Dishes</a>
+              <a href="#" className="py-2 pr-4">soup</a>
+              <a href="#" className="py-2 pr-4">Gill</a>
+            </nav>
+          </header>
+        </div>
+        <div className=" lg:col-span-2 fixed right-0 lg:static ">carrito</div>
+      </main>
     </div>
   );
 };
